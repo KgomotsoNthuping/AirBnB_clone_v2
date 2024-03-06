3b30f1a6669a1806735fb5a934f37dce421c3ec2
Kgomotso222 <kgomotsonthuping22@gmail.com>
2024-03-06 04:00:13 -0800

    dep

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Generate .tgz"""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """Generate .tgz"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
