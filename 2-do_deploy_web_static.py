1e42f105869c9b32128e50b58061b6100daa2899
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-03-07 20:14:18 +0200

    pyt

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,63 @@
 #!/usr/bin/python3
-"""
-distributes archive to web servers
-"""
+#This Fabfile to distribute an archive to a web server.
 
-from datetime import datetime
-from fabric.api import *
-import os
+import os.path
+from fabric.api import env, put, run
 
-env.hosts = ["44.210.150.159", "35.173.47.15"]
 env.user = "ubuntu"
-
-
-def do_pack():
-    """
-        do pack
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+env.hosts = ['18.209.180.135', '54.165.237.14']
 
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
     """
-        do deploy
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+    if os.path.isfile(archive_path) is False:
+        return False
+    fullFile = archive_path.split("/")[-1]
+    folder = fullFile.split(".")[0]
 
-        print("New version deployed!")
-        return True
+    if put(archive_path, "/tmp/{}".format(fullFile)).failed is True:
+        print("Uploading archive to /tmp/ failed")
+        return False
 
-    return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(folder)).failed is True:
+        print("Deleting folder with archive(if already exists) failed")
+        return False
+
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(folder)).failed is True:
+        print("Creating new archive folder failed")
+        return False
+
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(fullFile, folder)).failed is True:
+        print("Uncompressing archive to failed")
+        return False
+
+    if run("rm /tmp/{}".format(fullFile)).failed is True:
+        print("Deleting archive from /tmp/ directory dailed")
+        return False
+
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".
+           format(folder, folder)).failed is True:
+        print("Moving content to archive folder before deletion failed")
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(folder)).failed is True:
+        print("Deleting web_static folder failed")
+        return False
+
+    if run("rm -rf /data/web_static/current").failed is True:
+        print("Deleting 'current' folder failed")
+        return False
+
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(folder)).failed is True:
+        print("Creating new symbolic link to new code version failed")
+        return False
+
+    print("New version deployed!")
+    return True
